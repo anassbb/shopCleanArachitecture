0656ce925d5d01e256812a88b4a043792fd43bc8
anassbb <anasabassi11@gmail.com>
2023-07-13 11:59:27 +0200

    Interfaces

---
+++ ShopApp/ApplicationCore/Interfaces/IBasketService.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Entities.BasketAggregate;
+using Ardalis.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IBasketService
+    {
+        Task TransferBasketAsync(string anonymousId, string userName);
+        Task<Basket> AddItemToBasket(string username, int catalogItemId, decimal price, int quanntity = 1);
+        Task<Result<Basket>> SetQuantities(int basketId, Dictionary<string, int> quantities);
+        Task DeleteBasketAsync(int basketId);
+    }
+}
