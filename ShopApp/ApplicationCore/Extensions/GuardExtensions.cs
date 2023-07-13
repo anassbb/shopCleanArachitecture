a8481d5711cd8085e49936b30bd19084820e46a1
anassbb <anasabassi11@gmail.com>
2023-07-13 11:07:24 +0200

    Extension

---
+++ ShopApp/ApplicationCore/Extensions/GuardExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationCore.Entities.BasketAggregate;
+using ApplicationCore.Exceptions;
+using Ardalis.GuardClauses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Extensions
+{
+    public static class GuardExtensions
+    {
+
+        public static void EmptyBasketOnCheckOut(this IGuardClause guardClause, IReadOnlyCollection<BasketItem> basketItems)
+        {
+            if (!basketItems.Any())
+                throw new EmptyBasketOnCheckoutException();
+
+        }
+    }
+}
